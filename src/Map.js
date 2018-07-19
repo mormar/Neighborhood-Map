fa924bf6b773774cb9805dd9b8cbc93aa27164d5
Marcin <markar.mormar@gmail.com>
2018-07-19 22:04:04 +0200

    Tidy up code map add map style

--- src/Map.js
+++ src/Map.js
@@ -6,26 +6,22 @@ class Map extends Component {
 
   componentDidMount() {
       window.initMap = this.initMap;
-      loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyBlYjX2jC_PyB7Uo1E-lqnffUsySrZv3yY&callback=initMap')
+      loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyBlYjX2jC_PyB7Uo1E-lqnffUsySrZv3yY&callback=initMap');
   }
 
   initMap = function() {
-    var map;
-    map = new google.maps.Map(document.getElementById('map'), {
+    let map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 54.5053387, lng: 18.538661},
       zoom: 12
     });
   }
 
-
-
   render() {
 
-
     return (
-      <div style={{ height: '40rem', width: '100%' }}>
-        <h1>Gdynia pretty places</h1>
-        <div ref="map" id="map"></div>
+      <div>
+        <h1 id="main-taitel">Gdynia awesome places</h1>
+        <div id="map"></div>
       </div>
     );
   }
