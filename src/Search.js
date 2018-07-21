0a1f8d9caf2dd190648b9944ca1c58b8ef345b92
Marcin <markar.mormar@gmail.com>
2018-07-21 11:11:57 +0200

    Update search component

--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 
 class Search extends Component {
 
-   render() {
-     return (
-       <div>
-         
-       </div>
-     );
-   }
- }
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+   this.setState({ query: query })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="search-places">
+          <div className="search-places-bar">
+            <div className="search-places-input-wrapper">
+              <input type="text" placeholder="Search places"
+                value={this.state.query}
+                onChange={(event) => {
+                  this.updateQuery(event.target.value)
+                  }
+                }/>
+            </div>
+          </div>
+          <div className="search-places-results">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
