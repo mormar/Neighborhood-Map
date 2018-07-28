0c8e6a55eefe49d3cd2906ab3a17b2d6962821e2
Marcin <markar.mormar@gmail.com>
2018-07-28 15:32:01 +0200

    Creat main data storage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {combineReducers} from 'redux';
+import reducer from './reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const allReducers = combineReducers ({
+  date: reducer
+});
+
+const store = createStore(allReducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    {
+      id: 1
+    },
+    {
+      id: 2
+    }
+  ]
+}
