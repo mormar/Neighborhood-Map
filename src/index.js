439e4cf9133818df8abb45059d81fd5526a469ae
Marcin <markar.mormar@gmail.com>
2018-08-05 10:02:10 +0200

    Tidy up index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import {Provider} from 'react-redux';
-// import {createStore} from 'redux';
-// import {combineReducers} from 'redux';
-// import reducer from './reducer';
-// import ActivPlace from './ActivPlace';
-// import Search from './Search';
-// import Map from './Map';
 import { createStore, applyMiddleware } from 'redux';
 import { render } from "react-dom";
 import { Provider } from "react-redux";
@@ -26,20 +19,6 @@ render(
   document.getElementById('root')
 );
 
-// ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
 
 export default store;
-
-// const allReducers = combineReducers ({
-//   data: reducer,
-//   activePlace: ActivPlace
-// });
-// const store = createStore(allReducers);
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-//   , document.getElementById('root'));
-// registerServiceWorker();
